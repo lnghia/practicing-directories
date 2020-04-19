b3ec0e2e5d214737498058200e565b558c31f911
n3w-us3r <trongnghialedinh@gmail.com>
2020-04-19 16:58:41 +0700

    temp

--- cac thuat toan thong minh/tic-tac-toe/tictactoe.cpp
+++ cac thuat toan thong minh/tic-tac-toe/tictactoe.cpp
@@ -8,8 +8,7 @@
 
 using namespace std;
 
-class GameBoard
-{
+class GameBoard{
     vector<vector<char>> gb;
     vector<int> cache;
     pair<int, int> nextMove;
@@ -18,54 +17,44 @@ class GameBoard
     string vertical = "|", horizontal = "--- --- ---\n";
 
 public:
-    GameBoard()
-    {
+    GameBoard(){
         gb.resize(3, vector<char>(3, ' '));
         cache.resize(6, 0);
     }
 
-    int utility(int x, int y)
-    {
+    int utility(int x, int y){
         if (cache[x] == 3 || cache[y + 3] == 3 ||
             (gb[0][0] == 'X' && gb[1][1] == 'X' && gb[2][2] == 'X') ||
-            (gb[0][2] == 'X' && gb[1][1] == 'X' && gb[2][0] == 'X'))
-        {
-
+            (gb[0][2] == 'X' && gb[1][1] == 'X' && gb[2][0] == 'X')){
             return 1;
         }
         if (cache[x] == -3 || cache[y + 3] == -3 ||
             (gb[0][0] == 'O' && gb[1][1] == 'O' && gb[2][2] == 'O') ||
-            (gb[0][2] == 'O' && gb[1][1] == 'O' && gb[2][0] == 'O'))
-        {
-
+            (gb[0][2] == 'O' && gb[1][1] == 'O' && gb[2][0] == 'O')){
             return -1;
         }
 
-        return 0;
+        return (!count)?0:2;
     }
 
-    int min_val(int x, int y, int prev = INT_MIN)
-    {
-        if (isTerminated())
-        {
-            return utility(x, y);
+    int min_val(int x, int y, int prev = INT_MIN){
+        int util=utility(x, y);
+
+        if(util==1 || util==-1 || !util){
+            return util;
         }
 
         int v = INT_MAX, a, b, tmp;
 
-        for (int i = 0; i < 3; ++i)
-        {
-            for (int j = 0; j < 3; ++j)
-            {
-                if (gb[i][j] == ' ')
-                {
+        for (int i = 0; i < 3; ++i){
+            for (int j = 0; j < 3; ++j){
+                if (gb[i][j] == ' '){
                     gb[i][j] = 'O';
                     --cache[i];
                     --cache[j + 3];
                     --count;
                     tmp = max_val(i, j, v);
-                    if (v > tmp)
-                    {
+                    if (v > tmp){
                         v = tmp;
                         a = i;
                         b = j;
@@ -74,8 +63,7 @@ public:
                     ++cache[i];
                     ++cache[j + 3];
                     ++count;
-                    if (prev >= tmp)
-                    {
+                    if (prev >= tmp){
                         return tmp;
                     }
                 }
@@ -87,28 +75,24 @@ public:
         return v;
     }
 
-    int max_val(int x, int y, int prev = INT_MAX)
-    {
-        if (isTerminated())
-        {
-            return utility(x, y);
+    int max_val(int x, int y, int prev = INT_MAX){
+        int util=utility(x, y);
+
+        if(util==1 || util==-1 || !util){
+            return util;
         }
 
         int v = INT_MIN, a, b, tmp;
 
-        for (int i = 0; i < 3; ++i)
-        {
-            for (int j = 0; j < 3; ++j)
-            {
-                if (gb[i][j] == ' ')
-                {
+        for (int i = 0; i < 3; ++i){
+            for (int j = 0; j < 3; ++j){
+                if (gb[i][j] == ' '){
                     gb[i][j] = 'X';
                     ++cache[i];
                     ++cache[j + 3];
                     --count;
                     tmp = min_val(i, j, v);
-                    if (v < tmp)
-                    {
+                    if (v < tmp){
                         v = tmp;
                         a = i;
                         b = j;
@@ -117,8 +101,7 @@ public:
                     --cache[i];
                     --cache[j + 3];
                     ++count;
-                    if (prev <= tmp)
-                    {
+                    if (prev <= tmp){
                         return tmp;
                     }
                 }
@@ -130,12 +113,9 @@ public:
         return v;
     }
 
-    bool isTerminated()
-    {
-        for (auto &i : cache)
-        {
-            if (i == 3 || i == -3)
-            {
+    bool isTerminated(){
+        for (auto &i : cache){
+            if (i == 3 || i == -3){
                 return 1;
             }
         }
@@ -143,38 +123,32 @@ public:
         if ((gb[0][0] == 'X' && gb[1][1] == 'X' && gb[2][2] == 'X') ||
             (gb[0][2] == 'X' && gb[1][1] == 'X' && gb[2][0] == 'X') ||
             (gb[0][0] == 'O' && gb[1][1] == 'O' && gb[2][2] == 'O') ||
-            (gb[0][2] == 'O' && gb[1][1] == 'O' && gb[2][0] == 'O'))
-        {
+            (gb[0][2] == 'O' && gb[1][1] == 'O' && gb[2][0] == 'O')){
             return 1;
         }
 
         return !count;
     }
 
-    void askPlayerForNextMove()
-    {
-        while (1)
-        {
+    void askPlayerForNextMove(){
+        while (1){
             cout << "Enter your next move!\n";
             cout << "x: ";
             cin >> nextMove.first;
             cout << "y: ";
             cin >> nextMove.second;
-            if (nextMove.first < 0 || nextMove.first > 2 || nextMove.second < 0 || nextMove.second > 2)
-            {
+            if (nextMove.first < 0 || nextMove.first > 2 || nextMove.second < 0 || nextMove.second > 2){
                 system("clear");
                 draw();
                 cout << "x, y must be in range [0, 2]";
             }
-            else if (gb[nextMove.first][nextMove.second] != ' ')
-            {
+            else if (gb[nextMove.first][nextMove.second] != ' '){
                 system("clear");
                 draw();
                 cout << "Invalid move!\n";
                 cout << "You may made a made move!\n";
             }
-            else
-            {
+            else{
                 break;
             }
         }
@@ -185,8 +159,7 @@ public:
         --count;
     }
 
-    void bot()
-    {
+    void bot(){
         min_val(0, 0);
         gb[nextMove.first][nextMove.second] = 'O';
         --cache[nextMove.first];
@@ -194,38 +167,28 @@ public:
         --count;
     }
 
-    void draw()
-    {
+    void draw(){
         system("clear");
-        for (int i = 0; i < 3; ++i)
-        {
+        for (int i = 0; i < 3; ++i){
             cout << ' ' << gb[i][0] << ' ' << vertical << ' ' << gb[i][1] << ' ' << vertical << ' ' << gb[i][2] << '\n';
-            if (i != 2)
-            {
+            if (i != 2){
                 cout << horizontal;
             }
         }
     }
 
-    void gameLoop()
-    {
-        int util = 0;
-
+    void gameLoop(){
         cout << "Player is 'X'\n";
 
-        while (1)
-        {
+        while (1){
             draw();
-            if (isTerminated())
-            {
+            if (isTerminated()){
                 break;
             }
-            if (!turn)
-            {
+            if (!turn){
                 askPlayerForNextMove();
             }
-            else
-            {
+            else{
                 bot();
             }
             turn = !turn;
@@ -233,8 +196,7 @@ public:
     }
 };
 
-int main()
-{
+int main(){
     GameBoard game;
 
     game.gameLoop();
